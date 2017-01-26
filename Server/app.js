6faabb08ace1435c2324de8147d356d827596fbe
Romain <romain879@gmail.com>
2017-01-26 01:06:52 +0100

    First test

---
+++ Server/app.js
@@ -0,0 +1,156 @@
+var prompter    = require('./prompter.js');
+var net         = require('net');
+var shortId     = require('shortid');
+var colors      = require('colors');
+
+var clients = [];
+
+
+process.on('uncaughtException', function (err) {
+    for(var key in err) {
+        console.log('-----'+key+'-----');
+        console.log(err[key]);
+    }
+    //console.log(err);
+});
+
+function findClient(id) {
+    for(var key in clients) {
+        if( key === id || clients[key].name === id) {
+            return clients[key];
+        }
+    }
+    return null;
+}
+
+var server = net.createServer(function(socket) {
+
+
+
+    socket.name = shortId.generate();
+
+    clients.push(socket);
+
+    socket.write('Your name is ' + socket.name + '\n');
+    console.log('new client : ' + socket.name);
+
+    socket.buffer = '';
+
+    socket.on('data', function (data) {
+        var lines = data.toString().split(/\n|\r/),
+            i, line;
+
+        for (i = 0; i < lines.length - 1; i += 1) {
+            line = socket.buffer + lines[i];
+            socket.buffer = '';
+
+            if (line.length <= 510) {
+                socket.emit('message', line);
+            } else {
+                console.warn(socket.remoteAddress, socket.name,
+                    'Bufferoverflow');
+                socket.buffer = '';
+            }
+        }
+
+        socket.buffer += lines[lines.length - 1];
+        if (socket.buffer.length >= 510) {
+            console.warn(socket.remoteAddress, socket.name, 'Bufferoverflow.');
+            socket.buffer = '';
+        }
+    });
+
+    socket.on('message', function (msg) {
+        if (msg.trim() === '') {
+            return;
+        }
+        console.log(colors.yellow(socket.name) + colors.grey(' : ') + colors.white(msg));
+    });
+
+    socket.on('close', function() {
+        socket.emit('end', 'CLOSESOC :: ');
+    });
+    socket.on('end', function (msg) {
+        console.log(colors.red((msg||"")+socket.name + ' DISCONNECTED'));
+        clients.splice(clients.indexOf(socket), 1);
+    });
+
+
+});
+
+server.on('listening', function () {
+    console.log('Server listening');
+});
+
+server.on('close', function () {
+    console.log('Server is now closed');
+});
+
+server.on('error', function (err) {
+    console.log('error:', err);
+});
+
+server.listen(8088, '193.70.84.46');
+
+var App = (function() {
+
+    function App() {
+
+    }
+
+    App.prototype.query = function(str) {
+        var req = str.split(' ');
+        if(req[0] === 'clients') {
+            if(clients.length > 0) {
+                var ret = '';
+                ret += '-- Client list --\n';
+                for(var i=0; i<clients.length; i++) {
+                    ret += i + '\t\t'+clients[i].name+'\n';
+                }
+
+                console.log(colors.yellow(ret));
+            } else {
+                console.log(colors.yellow('no client connected'));
+            }
+        } else if (req[0] === 'send' && req[1] && req[2]) {
+            var cli = findClient(req[1]);
+            if(cli) {
+                var i = 2;
+                ret = '';
+                while(req[i]) {
+                    ret += req[i]+' ';
+                    i++;
+                }
+                cli.write(ret+'\n');
+                console.log(colors.gren('YOU')+colors.white(' >> ')+ colors.yellow(cli.name)+ colors.white(ret));
+            } else {
+                console.log(colors.yellow('Can\'t find this client...'));
+            }
+        } else if (req[0] === 'bc' && req[1]) {
+            var i = 2;
+            var ret = '';
+            while(req[i]) {
+                ret += req[i]+' ';
+                i++;
+            }
+            clients.forEach(function(c) {
+                c.write(ret+'\n');
+                console.log(colors.gren('YOU')+colors.white(' >> ')+ colors.yellow(c.name)+ colors.white(ret));
+            });
+        } else if (req[0] === 'kick' && req[1]) {
+            var cli = findClient(req[1]);
+            if(cli) {
+                cli.emit('end');
+            } else {
+                console.log(colors.yellow('Can\'t find this client...'));
+            }
+        }
+    };
+
+    return App;
+})();
+
+var app = new App();
+
+prompter(app);
+
---
+++ Server/prompter.js
@@ -0,0 +1,96 @@
+/*
+ *
+ * IMPORTS
+ */
+
+var colors      = require('colors');
+var readline    = require('readline');
+var util        = require('util');
+
+module.exports = function(app) {
+
+    /*
+     *
+     * CONST
+     */
+
+    // command line before cmd
+    const cmdHead   = colors.grey('$socket ')+colors.green('> ');
+
+
+
+
+    const clear = function() {
+        process.stdout.write("\x1Bc");
+    };
+
+    const fu = function (type, args) {
+        var t = Math.ceil((rl.line.length + 3) / process.stdout.columns);
+        var text = util.format.apply(console, args);
+        rl.output.write("\n\x1B[" + t + "A\x1B[0J");
+        rl.output.write(text + "\n");
+        rl.output.write(new Array(t).join("\n\x1B[E"));
+        rl._refreshLine();
+    };
+
+    const completer = function(line) {
+        var completions = [
+            ''
+        ];
+
+         var hits = completions.filter(function(c) {
+             return c.indexOf(line) == 0;
+         });
+
+         if (hits.length > 0) {
+             return [hits, line];
+         }
+         return [[], line];
+    };
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        completer: completer
+    });
+    /*
+     *
+     * Events
+     */
+    rl.on("line", function(line) {
+
+        app.query(line);
+
+        rl.prompt();
+    });
+    rl.on('close', function() {
+        return process.exit(0);
+    });
+    rl.on("SIGINT", function() {
+        rl.clearLine();
+        return process.exit(0);
+    });
+
+
+
+    console.log = function() {
+        fu("log", arguments);
+    };
+    console.warn = function() {
+        fu("warn", arguments);
+    };
+    console.info = function() {
+        fu("info", arguments);
+    };
+    console.error = function() {
+        fu("error", arguments);
+    };
+
+    /*
+     *
+     * RUN
+     */
+    clear();
+    rl.setPrompt(cmdHead, 2);
+    rl.prompt();
+};
+
