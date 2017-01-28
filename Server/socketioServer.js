807f4c139307543b77f1eea8c10055f032628893
agrobost <grahulte@gmail.com>
2017-01-28 13:51:49 +0100

    osef

--- Server/socketioServer.js
+++ Server/socketioServer.js
@@ -4,45 +4,41 @@ var fs = require('fs');
 module.exports = SocketioServer;
 
 
-function SocketioServer(centralizedServer){
-    var socketioServer = http.createServer(function(req, res) {
-        fs.readFile('../Webclient/index.html', 'utf-8', function(error, content) {
+function SocketioServer(centralizedServer) {
+    var socketioServer = http.createServer(function (req, res) {
+        fs.readFile('../Webclient/index.html', 'utf-8', function (error, content) {
             res.writeHead(200, {"Content-Type": "text/html"});
             res.end(content);
         });
     });
-
     var io = require('socket.io').listen(socketioServer);
 
-    // Quand un client se connecte, on le note dans la console
     io.sockets.on('connection', function (socket) {
 
         //we create socket.write in socketio, to emit easly since centralizedServer
-        socket.write = function(message){
-            socket.emit("data",message);
+        socket.write = function (message) {
+            socket.emit("data", message);
         };
 
         centralizedServer.onConnect(socket);
 
         //meme si avec socketio on peut faire socket.on("machintruc"), on en créé qu'un seul : on("data") pour que ce soit pareil que javaServer.js c plus simple, on gere le reste dans centralized Server
-        socket.on('data',function(data){
+        socket.on('data', function (data) {
             //La méthode trim() permet de retirer les blancs en début et fin de chaîne.
             var dataParts = data.toString().trim().split(' ');
 
             //part 0 correspond to the identifiant of the message, for example : /w /setNicknname, ....
             var cmd = dataParts[0];
             var message = "";
-            for(var i = 1;i<dataParts.length;i++){
-                message += dataParts[i]+" ";
+            for (var i = 1; i < dataParts.length; i++) {
+                message += dataParts[i] + " ";
             }
-            centralizedServer.onData(socket,cmd,message.trim());
+            centralizedServer.onData(socket, cmd, message.trim());
         });
 
-        socket.on("disconnect", function(){
+        socket.on("disconnect", function () {
             centralizedServer.onDisconnect(socket);
         });
     });
-
-
     socketioServer.listen(8087);
 }
