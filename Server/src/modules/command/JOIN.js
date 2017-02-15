ba5164976957839d7d62546cc105bb6439f121fc
Romain <romain879@gmail.com>
2017-02-15 18:03:15 +0100

    fix JOIN command when user create channel

--- Server/src/modules/command/JOIN.js
+++ Server/src/modules/command/JOIN.js
@@ -12,26 +12,19 @@ module.exports = function (socket, command) {
     let name = command[1].split(' ')[0];
     let key = command[1].split(' ')[1] || '';
 
-    if (name[0] !== '#') {
+    if(name.match(/\W/g).join('') !== '#' || name[0] !== '0' || name.length <= 1) {
         ERRSender.ERR_NOSUCHCHANNEL(socket.client, {name: name});
         return;
     }
 
     let err = true;
-
     Channel.list().forEach((channel) => {
         if (channel.name === name) {
             err = false;
-            // join
             channel.addUser(socket.client, key);
         }
     });
     if (err) {
-        // create
-        if (name.match(/(!^#)|(^G)|,/g) || name.length >= 50 || name ==='') {
-            ERRSender.ERR_NOSUCHCHANNEL(creator, {name: name});
-            return;
-        }
         new Channel(socket.client, name, key, 20);
     }
 
