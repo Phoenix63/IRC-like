40fb126d04564a226296994aa10731582df64496
Neotrix <titiroulio@hotmail.fr>
2017-02-15 14:26:39 +0100

    add Whois command

--- Server/src/modules/command/WHOIS.js
+++ Server/src/modules/command/WHOIS.js
@@ -1,13 +1,15 @@
 /**
  * Created by titir on 10/02/2017.
  */
-
+ /* <server.ip> 311 <user.nick> <user.identity> <server.ip> * :<user.realname> */
 import ERRSender from './../responses/ERRSender';
+import config from './../../config.json';
 
 module.exports = function(socket,command) {
     if (! socket.client.isRegistered) {
         ERRSender.ERR_NOTREGISTERED(socket.client, "WHOIS");
         return;
     }
-    socket.send(command[1]);
+
+    socket.send("crismos.fr"+" "+"311"+" "+socket.client.name+" "+socket.client.identity+" "+config.ip+" :"+socket.client.realname);
 }
