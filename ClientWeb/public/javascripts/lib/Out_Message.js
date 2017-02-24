e5c29e38520aad072d0c6d430a4491f375ab2ff8
GiJiang <gillesjiang1@gmail.com>
2017-02-24 01:40:02 +0100

    refactoring

--- ClientWeb/public/javascripts/lib/Out_Message.js
+++
@@ -1,8 +0,0 @@
-function out_isMsg(msg) {
-	var msSplit = msg.split(' ');
-	var msToSend = "";
-	for(var i = 2; i<msSplit.length; i++) {
-		msToSend = msToSend + " " + msSplit[i];
-	}
-	return msToSend;
-}
