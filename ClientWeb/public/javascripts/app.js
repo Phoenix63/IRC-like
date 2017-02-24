8459f5fa9d110c33dd5d294e821005f9bfaded7f
GiJiang <gillesjiang1@gmail.com>
2017-02-24 16:54:01 +0100

    change route with angular js

--- ClientWeb/public/javascripts/app.js
+++ ClientWeb/public/javascripts/app.js
@@ -1,1 +1,26 @@
-var myApp = angular.module('myApp',[]);
+'use strict';
+var myApp = angular.module('myApp',['ngAnimate',
+    'ngRoute']);
+
+myApp.config(function ($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+    $routeProvider
+        .when('/', {
+            templateUrl: 'html/log_in.html'
+        })
+        .when('/sign_in', {
+            templateUrl: 'html/sign_in.html'
+        })
+        .when('/irc', {
+            templateUrl: 'html/irc.html'
+        })
+        .when('/login', {
+            templateUrl: 'html/log_in.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
