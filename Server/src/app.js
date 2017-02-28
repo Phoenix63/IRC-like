4408189ec0af0bd1978b674e8c0a1e0c2e916385
Romain <romain879@gmail.com>
2017-02-28 12:59:53 +0100

    set Restart command restarting all process child and reloading db

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,98 +1,29 @@
-"use strict";
+import child_process from 'child_process';
 
-process.title = 'server';
+const env = process.argv[2] || 'PROD';
+console.log(env);
 
-console.log = function(arg) {
-    process.stdout.write(arg+'\n');
-};
+let childprocess = createChild();
 
-import dbSaver from './modules/data/dbSaver';
-
-// globals
-import colors from 'colors';
-
-// socket
-import socketManager from './modules/socket/socket';
-import Client from './modules/client/client';
-import Logger from './modules/Logger';
-import MessageManager from './modules/CommandManager';
-import RPLSender from './modules/responses/RPLSender';
-import dbLoader from './modules/data/dbLoader';
-
-import cluster from 'cluster';
-
-const numCPUs = require("os").cpus().length;
-
-let quiting = false;
-function quitHandle(e, callback=function(){}) {
-    if(e) {
-        console.log(e);
+process.on('SIGINT', () => {
+    if(childprocess && childprocess.kill) {
+        createChild = null;
+        childprocess.kill(0);
     }
-    if(!quiting) {
-        quiting = true;
-        console.log('saving database...');
-        dbSaver(false, () => {
-            console.log('database saved!');
-            for(let id in cluster.workers) {
-                cluster.workers[id].kill();
-            }
-            callback();
-            process.exit(0);
-        });
-    }
-}
+});
 
+function createChild() {
+    let child = child_process.spawn('node', ['./dist/server.js', env]);
+    child.stdout.on('data', function (data) {
+        console.log(data.toString());
+    });
 
-if(cluster.isMaster) {
-    dbLoader(() => {
-        console.log('Database loaded!');
-        for(let i = 0 ; i<numCPUs; i++) {
-            cluster.fork();
+    child.on('exit', function (code) {
+        console.log('child process exited with code ' + code.toString());
+        if(createChild) {
+            child = createChild();
         }
     });
 
-    process.on('exit', quitHandle);
-
-    process.on('SIGINT', quitHandle);
-
-    if (!(process.argv[2] && (process.argv[2] === 'DEV' || process.argv[2] === 'TEST'))) {
-        process.on('uncaughtException', (err) => {
-            console.log('\t\t' + colors.red(err));
-        });
-    } else {
-        process.on('uncaughtException', quitHandle);
-    }
-
-    cluster.on('message', (worker, msg) => {
-        if(msg && msg.quitmessage) {
-            console.log('restart...');
-            for(let key in cluster.workers) {
-                cluster.workers[key].kill();
-            }
-            cluster.worker.kill();
-        }
-    });
-    cluster.on('exit', (worker, code, signal) => {
-        console.log('worker '+worker.process.pid+' died');
-        cluster.fork();
-    });
-
+    return child;
 }
-if(cluster.isWorker) {
-    socketManager.create((socket) => {
-        let client = new Client(socket);
-        let logger = new Logger(client);
-        client.socket.logger = logger;
-        client.socket.messageManager = new MessageManager(client.socket);
-        socket.on('connect', () => {
-            RPLSender.HEADER(socket);
-            logger._CLIENT_CONNECTED();
-        });
-    });
-}
-
-
-
-
-
-
