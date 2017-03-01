2cc7f819e97c4a83705c37fa418b502b43c04243
agrobost <grahulte@gmail.com>
2017-03-01 10:20:24 +0100

    MODE UnitTest

--- Server/test/02_mode.js
+++ Server/test/02_mode.js
@@ -37,6 +37,34 @@ describe("command MODE:", () => {
             done();
         });
     });
+    it('should say you\'re not channel operator', (done) => {
+        client1 = new Client(config.port, config.ip);
+
+        client1.on('connect', () => {
+            client1.send('NICK user1');
+            client1.send('USER user1 0 * :user1');
+        });
+        client1.on('auth', () => {
+            client1.send('JOIN #chan33');
+            client2 = new Client(config.port, config.ip);
+            client2.on('connect', () => {
+                client1.send('NICK user2');
+                client2.send('USER user2 0 * :user2');
+            });
+            client2.on('auth', () => {
+                client2.send('JOIN #chan33');
+                client2.send('MODE #chan33 +l 27');
+            });
+            client2.on('err_chanoprivneeded', () => {
+                client2.close();
+                client1.close();
+                done();
+            });
+        });
+
+
+
+    });
 
 
 });
--- Server/test/core/Client.js
+++ Server/test/core/Client.js
@@ -36,6 +36,7 @@ var Client = (function() {
     }
 
     Client.prototype.parse = function(message) {
+        //console.log(message);
         if(message.indexOf('QUIT') < 0) {
 
             this.emit('message', message);
@@ -86,12 +87,7 @@ var Client = (function() {
             else if (message.indexOf('501') > 0){
                 this.emit('err_unknownflag',message);
             }
-            else if (message.indexOf('') > 0){
-                this.emit('',message);
-            }
-            else if (message.indexOf('') > 0){
-                this.emit('403',message);
-            }
+
             /*
             else if (message.indexOf('') > 0){
                 this.emit('',message);
