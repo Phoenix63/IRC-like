f988df65e27557ffb34897eddb0f5a376591df78
VoidSide <metalie@hotmail.fr>
2017-03-24 15:11:11 +0100

    Log in
    Log in final

--- ClientWeb/public/javascripts/lib/menu.js
+++ ClientWeb/public/javascripts/lib/menu.js
@@ -1,37 +1,9 @@
-
 $(document).ready(function(){
 
 	/*$("#test").click(function(){
 		$("#menu_emoji").slideDown();
 	});*/
 
-
-    $('.bouton_emoji').click(function(){
-
-        $("#menu_emoji").slideToggle(50);
-
-        $("#inputSendMessage").focus();
-
-    });
-
-    $('.emoticon').click(function(){
-		
-		var textarea_val = jQuery.trim($('#inputSendMessage').val());
-		var emoticon_val = $(this).attr('value');
-
-		if (textarea_val == '') {
-			var sp = '';
-		} else {
-			var sp = ' ';
-		}
-
-		/*if(emoticon_val == ':)'){
-			$("<span></span>").html('<img src="img/smile.png" alt="">').appendTo('.messInBox');
-		}*/
-
-		$('#inputSendMessage').focus().val(jQuery.trim(textarea_val + sp + emoticon_val + sp));
-		//$('#div1').focus().val(jQuery.trim(div_val + ' ' + emoticon_val + ' '));
-	});
 
 
 	$("#fleche").click(function(){
